3f661e78e547a294dadb48be38aa02c4d5769466
Alejandro González <contacto@agonzalezr.com>
2022-11-17 22:23:03 -0600

    Fix timer

--- app/components/Player/index.tsx
+++ app/components/Player/index.tsx
@@ -4,8 +4,6 @@ import {useEffect, useState} from "react";
 import {Container} from "./style";
 import {song} from "../../config"
 
-const timeouts: Array<any> = []
-
 const timestamps = [
     "00:08",
     "00:12",
@@ -13,56 +11,19 @@ const timestamps = [
     "00:20",
 ]
 
-export const Player = () => {
+export const Player = ({name, age}) => {
 
     const [currentLine, setCurrentLine] = useState(0)
 
-
-    const handleCurrentLine = () => {
-        setCurrentLine(currentLine+1)
-    }
-
-    // useEffect(()=>{
-    //     for (let i = 0; i < timestamps.length; i++) {
-    //         (function(index) {
-    //             console.log(mtm(timestamps[index])/1000)
-    //             let timeout = setTimeout(handleCurrentLine, mtm(timestamps[index]));
-    //             timeouts.push(timeout)
-    //         })(i);
-    //     }
-    //     return () => {
-    //         clearTimeout(timeouts[currentLine-1])
-    //     }
-    // },[])
-
-    // for (let i = 0; i < timestamps.length; i++) {
-    //     (function(index) {
-    //         console.log(mtm(timestamps[index])/1000)
-    //         setTimeout(handleCurrentLine, mtm(timestamps[index]));
-    //     })(i);
-    // }
-
-    // timestamps.forEach((time)=>{
-    //     setTimeout(handleCurrentLine, mtm(time))
-    // })
-
-    // for(let i=0; i<timestamps.length; i++)
-    // {
-    //     setTimeout(handleCurrentLine, mtm(timestamps[i]))
-    // }
-    useEffect(() => {
-        setTimeout(handleCurrentLine, mtm(timestamps[0]))
-    }, []);
-
-    // useEffect(() => {
-    //     setTimeout(handleCurrentLine, mtm(timestamps[currentLine]))
-    // }, [currentLine]);
-
-    // useEffect(()=>{
-    //     return () => {
-    //         clearTimeout(timeouts[currentLine-1])
-    //     }
-    // },[currentLine])
+    useEffect(()=>{
+        function updateState() {
+            setCurrentLine(currentLine + 1)
+        }
+        let timer = setTimeout(updateState, mtm(timestamps, currentLine))
+        return () => {
+            clearTimeout(timer)
+        }
+    },[currentLine])
 
     return (
         <Container>
@@ -71,7 +32,7 @@ export const Player = () => {
                     <h1>{song[currentLine]}</h1>
                 </div>
             </div>
-            {/*<ReactAudioPlayer src="/audio/song.mp3" autoPlay/>*/}
+            <ReactAudioPlayer src="/audio/song.mp3" autoPlay/>
         </Container>
     )
 }
--- app/functions/minutesToMilliseconds.ts
+++ app/functions/minutesToMilliseconds.ts
@@ -1,4 +1,12 @@
-export const minutesToMilliseconds = (minute: string) => {
-    let [minutes, seconds] = minute.split(":")
-    return (+minutes*(60000)) + (+seconds*1000)
+
+export const minutesToMilliseconds = (timestamps: Array<string>, line: number) => {
+    const timers = []
+    timestamps.forEach((time: string)=>{
+        let [minutes, seconds] = time.split(":")
+        timers.push((+minutes*(60000)) + (+seconds*1000))
+    })
+    if(line === 0)
+        return timers[0]
+
+    return timers[line] - timers[line-1]
 }
